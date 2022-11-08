4336f28c142b51dd497c9571e51fa6c95cd59abb
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2022-11-08 17:37:17 +0900

    Setting: model schema 생성

---
+++ src/model/user.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, min: 8, max: 20 },
+    name: { type: String, required: true, max: 16 },
+    countryCode: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    agreeTerms: { type: Boolean, require: true, default: false },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model('User', userSchema);
